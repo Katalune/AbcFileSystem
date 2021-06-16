e8060dff6f674433c9e51223d60857c46ede1b91
Kata Zavorotchenko <kata@fb.com>
2021-06-16 13:29:31 +0100

    add file system

---
+++ src/main/kotlin/org/jetbrains/plugins/template/AbcFileSystem.kt
@@ -0,0 +1,44 @@
+package org.jetbrains.plugins.template
+
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.impl.ArchiveHandler
+import com.intellij.openapi.vfs.newvfs.ArchiveFileSystem
+
+object AbcFileSystem : ArchiveFileSystem() {
+  val PROTOCOL = "abc"
+  override fun getProtocol(): String {
+    return PROTOCOL
+  }
+
+  override fun findFileByPath(path: String): VirtualFile? {
+    TODO("Not yet implemented")
+  }
+
+  override fun refresh(asynchronous: Boolean) {
+    TODO("Not yet implemented")
+  }
+
+  override fun refreshAndFindFileByPath(path: String): VirtualFile? {
+    TODO("Not yet implemented")
+  }
+
+  override fun extractRootPath(normalizedPath: String): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun findFileByPathIfCached(path: String): VirtualFile? {
+    TODO("Not yet implemented")
+  }
+
+  override fun extractLocalPath(rootPath: String): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun composeRootPath(localPath: String): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun getHandler(entryFile: VirtualFile): ArchiveHandler {
+    TODO("Not yet implemented")
+  }
+}
