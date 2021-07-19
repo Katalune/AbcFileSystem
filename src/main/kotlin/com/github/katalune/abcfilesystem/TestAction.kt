923fe070d3402751dfb2450ec6bac94ba3bca62f
Kata Zavor <kata@fb.com>
2021-07-20 00:33:59 +0100

    Add more fileSystem examples

--- src/main/kotlin/com/github/katalune/abcfilesystem/TestAction.kt
+++ src/main/kotlin/com/github/katalune/abcfilesystem/TestAction.kt
@@ -9,6 +9,7 @@ import com.intellij.psi.PsiManager
 class TestAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         /*
+        * ApkFileSystem
         * Example of existing File System: https://github.com/intellij-rust/intellij-rust/blob/master/src/main/resources/META-INF/rust-core.xml
         * <indexedRootsProvider implementation="org.rust.lang.core.macros.RsIndexableSetContributor"/>
         * <virtualFileSystem key="rust_macros" implementationClass="org.rust.lang.core.macros.MacroExpansionFileSystem"/>
@@ -17,6 +18,7 @@ class TestAction: AnAction() {
         <filetype.prebuiltStubsProvider filetype="Rust"
                                         implementationClass="org.rust.lang.core.macros.MacroExpansionStubsProvider"/>
         */
+        // ArhiveFileSystem#getRootByLocal file:///Users/kata/.gradle/caches/transforms-1/files-1.1/runtime-1.1.0.aar/fa4726cf6dd7f7a148c9fb0b13e46bb5/jars/classes.jar
         // indexable roots contain jar:///Users/kata/IdeaProjects/AbcFileSystem/.jar!/schemas
         printFile("abc://" + e.project?.basePath + "/app/libs/arc.abc" + "!/Some.kt", e.project!!)
     }
